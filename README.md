4fb6dc4eea1800064a189514ed9361e24544e2d5
Kamal Nasser <hello@kamal.io>
2017-12-10 16:46:35 +0200

    qsjson

---
+++ README.md
@@ -0,0 +1,7 @@
+qsjson is a microservice that returns the query string as a JSON object.
+
+**usage**
+
+- grab a binary from the releases page
+- run it (`qsjson` or `qsjson -listenAddr localhost:8000`)
+- `curl 'http://localhost:3000/?test=true&json=ok'`
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+)
+
+var listenAddress = flag.String("listenAddress", "localhost:3000", "address to run the HTTP server on")
+
+func flatten(inp map[string][]string) map[string]string {
+	ret := make(map[string]string, len(inp))
+	for k, v := range inp {
+		ret[k] = v[0]
+	}
+
+	return ret
+}
+
+func marshalQueryString(w http.ResponseWriter, r *http.Request) {
+	obj, err := json.Marshal(flatten(r.URL.Query()))
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("internal server error"))
+		return
+	}
+
+	w.Write(obj)
+}
+
+func main() {
+	http.HandleFunc("/", marshalQueryString)
+	log.Printf("listening on %s\n", *listenAddress)
+	err := http.ListenAndServe(*listenAddress, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
